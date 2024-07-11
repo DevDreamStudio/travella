3e1356814a7b486d0e73f8b7169386fdd245f344
hell4ire <hell4ire@gmail.com>
2024-07-11 20:04:38 +0500

    add travella

---
+++ layouts/default.vue
@@ -0,0 +1,9 @@
+<template>
+    <NuxtPage :links="links" />
+</template>
+
+<script setup lang="ts">
+const links: Array<object> = [
+    { name: 'Home', url: '/' },
+];
+</script>
---
+++ pages/index/components/Header.vue
@@ -0,0 +1,11 @@
+<template>
+    <header class="header">
+        <div class="header__content">
+            <NuxtImg src="/assets/images/2.png" alt="Description of the image" />
+        </div>
+    </header>
+</template>
+
+<script setup lang="ts">
+
+</script>
