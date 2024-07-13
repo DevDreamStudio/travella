f566ba6a5370c5229272d0d614b2d2484fcb47db
gffdg <aliavita.developers@gmail.com>
2024-07-13 11:47:27 +0500

    fix

--- pages/index/components/Header.vue
+++ pages/index/components/Header.vue
@@ -1,11 +1,23 @@
 <template>
-    <header class="header">
-        <div class="header__content">
-            <NuxtImg src="/assets/images/2.png" alt="Description of the image" />
-        </div>
-    </header>
+  <header class="header">
+    <div class="header__content">
+      <img
+        class="header__content-bg"
+        src="assets/images/2.png"
+        width="100%"
+        height="744px"
+        loading="lazy"
+        alt="header-bg"
+      />
+      <div class="header__content-text">
+        <h1 class="header__content-text-title">
+          <span>Лучшие Туры для Вас:</span> Путешествуйте с Комфортом и
+          Безопасностью
+        </h1>
+      </div>
+    </div>
+  </header>
 </template>
 
 <script setup lang="ts">
-
 </script>
